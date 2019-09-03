2ccaea7986520b4e0ce6b953fbc7b87d67c2d43e
kj <kristoferjoseph@gmail.com>
2019-09-03 11:41:22 -0700

    Making lint happy.

--- src/http/post-woops/index.js
+++ src/http/post-woops/index.js
@@ -1,7 +1,7 @@
 // learn more about http functions here: https://arc.codes/guides/http
-exports.handler = async function http(req) {
+exports.handler = async function http() {
   return {
-    headers: {'content-type': 'text/html; charset=utf8'}, 
-    body: '<b>hello world from nodejs<b>'
+    headers: {'content-type': 'text/html; charset=utf8'},
+    body: '<b>POST WOOPS<b>'
   }
 }
